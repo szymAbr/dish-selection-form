6f7166f46f8ba24862f01dea8ba6be61b47445ad
szymAbr <sz.abramowski@gmail.com>
2022-06-18 17:42:40 +0200

    add styles

---
+++ src/components/styles/Buttons.styled.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Buttons = styled.div`
+  margin-top: 1rem;
+
+  & button {
+    margin: 0.5rem;
+  }
+`;
---
+++ src/components/styles/Select.styled.ts
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const Select = styled.select`
+  margin-top: 1rem;
+`;
