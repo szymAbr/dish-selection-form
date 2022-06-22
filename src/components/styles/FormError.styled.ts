fc7560e7be954987a96a899c67008688e6299c5d
szymAbr <sz.abramowski@gmail.com>
2022-06-22 22:52:45 +0200

    update styles

--- src/components/styles/FormError.styled.ts
+++ src/components/styles/FormError.styled.ts
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 
 export const FormError = styled.span`
-  font-size: 1.5rem;
+  font-size: 0.9rem;
+  font-weight: 700;
   color: red;
 `;
--- src/components/styles/FormPaper.styled.ts
+++ src/components/styles/FormPaper.styled.ts
@@ -4,10 +4,10 @@ import styled from "@emotion/styled";
 export const FormPaper = styled(Paper)`
   max-width: 80%;
   margin: 4rem 0;
-  padding: 3rem;
+  padding: 4rem 3rem;
 
   @media (min-width: 700px) {
-    padding: 7rem;
+    padding: 4rem 7rem;
     margin: 2rem 0;
   }
 `;
